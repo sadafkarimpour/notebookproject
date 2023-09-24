9fbe43ee201c5d7ab39538f0226602aad9b7fdd9
Sadaf K. <sadaf.kp81@gmail.com>
2023-09-24 16:25:50 +0330

    change part3

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
                 <a href="<?php echo PATH."index.php"?>" class="text-white  align-middle text-center">Home</a>
             </div>
             <div class="col-lg-2 col-md-2 col-sm-1  align-middle text-center ">
-                <a href="<?php echo PATH."note.php"?>" class="text-white  align-middle text-center">Notes</a>
+                <a href="<?php echo PATH."note.php?action=index"?>" class="text-white  align-middle text-center">Notes</a>
             </div>
             <div class="col-lg-2 col-md-2 col-sm-1  align-middle text-center"> 
                 <a href="<?php echo PATH."auth.php?action=login"?>" class="text-white  align-middle text-center">Login</a>
